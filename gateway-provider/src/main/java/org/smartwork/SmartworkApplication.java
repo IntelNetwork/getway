0ee6fb7223a464eb3d6753d57ea03d61e4a760df
Hank.huang <Hank.huang@shdbr.com>
2020-02-25 20:56:52 +0800

    '网关首次提交'

--- gateway-provider/src/main/java/org/smartwork/SmartworkApplication.java
+++ gateway-provider/src/main/java/org/smartwork/SmartworkApplication.java
@@ -1,17 +1,21 @@
 package org.smartwork;
 
+import org.forbes.comm.constant.CommonConstant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(scanBasePackages={"org.forbes","org.smartwork"},exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages={"org.forbes","org.smartwork"})
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = {"org.forbes.comm.service"})
+@EnableZuulProxy
 public class SmartworkApplication {
 
     public static void main(String[] args) {
+        System.setProperty(CommonConstant.PROVILES_CODE,"prod");
         SpringApplication.run(SmartworkApplication.class, args);
     }
 }
